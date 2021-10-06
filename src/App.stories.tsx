8525fa1ec73df0172b5f8d1955eb2d4717de4ea4
AlexBalew <balew48@gmail.com>
2021-10-06 21:49:17 +0300

    Storybook has been implemented to the project. There are several stories were created: App, Task, EditableSpan & AddItemForm. Fixed bug with tasks filtration.

---
+++ src/App.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ComponentMeta, Story} from '@storybook/react';
+import App from "./App";
+import {ReduxStoreProviderDecorator} from "./stories/Decorators/ReduxStoreProviderDecorator";
+
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'APPExample',
+    component: App,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof App>;
+
+
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: Story = () => <App />;
+
+export const AppExample = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+AppExample.args = {
+
+};
---
+++ src/EditableSpan.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import {action} from "@storybook/addon-actions";
+import {EditableSpan} from "./EditableSpan";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'EditableSpanExample',
+    component: EditableSpan,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        onChange: {
+            description: 'Value was changed'
+        },
+        title: {
+           defaultValue: 'DoubleClick me',
+           description: 'Default value of  EditableSpan'
+        }
+    },
+} as ComponentMeta<typeof EditableSpan>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof EditableSpan> = (args) => <EditableSpan {...args} />;
+
+export const EditableSpanExample = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+EditableSpanExample.args = {
+   onChange: action('Value was changed')
+};
