87da485bb9902e8e9eaff8a646230a74fea58534
AlexBalew <balew48@gmail.com>
2021-10-15 13:37:03 +0300

    Some APIs methods for Tasks created for the storybook demo(createTask, deleteTask, updateTask)

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -84,7 +84,42 @@ export const CreateTask = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        tasksAPI.createTask('7b808f03-58c9-4db8-8972-e219d67bad19', 'yoyoyoy')
+        tasksAPI.createTask(
+            '7b808f03-58c9-4db8-8972-e219d67bad19',
+            'YOYOYO')
+            .then((res) => {
+                setState(res.data)
+            })
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const DeleteTaskFunc = () => {
+    const [state, setState] = useState<any>(null)
+
+    useEffect(() => {
+        tasksAPI.deleteTask(
+            '7b808f03-58c9-4db8-8972-e219d67bad19',
+            'c812d653-7c52-41f0-bf85-815de5759702')
+            .then((res) => {
+                setState(res.data)
+            })
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTask = () => {
+    const [state, setState] = useState<any>(null)
+
+    useEffect(() => {
+        tasksAPI.updateTask(
+            '7b808f03-58c9-4db8-8972-e219d67bad19',
+            '7d2b5ee5-3bad-4fe1-8ac1-cf8d3567b988',
+            {title: 'YYYYYY', description: null, deadline: null, priority: 2, startDate: null, status: 8})
             .then((res) => {
                 setState(res.data)
             })
